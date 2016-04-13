59cbf44c21d1d7b5e1238d438e31e458ff76faac
Courtney Burns <courtneylynnburns@gmail.com>
2016-04-13 10:27:42 -0500

    model changes

---
+++ app/dashboards/venue_dashboard.rb
@@ -0,0 +1,72 @@
+require "administrate/base_dashboard"
+
+class VenueDashboard < Administrate::BaseDashboard
+  # ATTRIBUTE_TYPES
+  # a hash that describes the type of each of the model's fields.
+  #
+  # Each different type represents an Administrate::Field object,
+  # which determines how the attribute is displayed
+  # on pages throughout the dashboard.
+  ATTRIBUTE_TYPES = {
+    event: Field::BelongsTo,
+    id: Field::Number,
+    name: Field::String,
+    created_at: Field::DateTime,
+    updated_at: Field::DateTime,
+    address: Field::String,
+    phone_number: Field::String,
+    website: Field::String,
+    city: Field::String,
+    zip: Field::String,
+    state: Field::String,
+  }
+
+  # COLLECTION_ATTRIBUTES
+  # an array of attributes that will be displayed on the model's index page.
+  #
+  # By default, it's limited to four items to reduce clutter on index pages.
+  # Feel free to add, remove, or rearrange items.
+  COLLECTION_ATTRIBUTES = [
+    :event,
+    :id,
+    :name,
+    :created_at,
+  ]
+
+  # SHOW_PAGE_ATTRIBUTES
+  # an array of attributes that will be displayed on the model's show page.
+  SHOW_PAGE_ATTRIBUTES = [
+    :event,
+    :id,
+    :name,
+    :created_at,
+    :updated_at,
+    :address,
+    :phone_number,
+    :website,
+    :city,
+    :zip,
+    :state,
+  ]
+
+  # FORM_ATTRIBUTES
+  # an array of attributes that will be displayed
+  # on the model's form (`new` and `edit`) pages.
+  FORM_ATTRIBUTES = [
+    :event,
+    :name,
+    :address,
+    :phone_number,
+    :website,
+    :city,
+    :zip,
+    :state,
+  ]
+
+  # Overwrite this method to customize how venues are displayed
+  # across all pages of the admin dashboard.
+  #
+  # def display_resource(venue)
+  #   "Venue ##{venue.id}"
+  # end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
+  namespace :admin do
+    resources :users
+resources :bands
+resources :events
+resources :venues
+
+    root to: "users#index"
+  end
+
   get "events/list" => 'events#list'
   get "events/details/:date" => 'events#details'
   devise_for :users
