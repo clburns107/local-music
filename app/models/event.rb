033defcf57b2959f75fb17e68cdd9d56727e6fbb
Courtney Burns <courtneylynnburns@gmail.com>
2016-04-22 15:37:20 -0500

    deleted route to see if that helped

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,9 +3,5 @@ class Event < ActiveRecord::Base
   belongs_to :band
 
   require 'Date'
-  
-  def self.this_week
-    puts 'class method'
-  end
-  
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,6 @@ Rails.application.routes.draw do
   get "events/list" => 'events#list'
   get "events/details/:date" => 'events#details'
   get "static" => 'static#playground'
-  get "events/lastweek" => 'events#lastweek'
   devise_for :users
   root to: "events#list"
 end
